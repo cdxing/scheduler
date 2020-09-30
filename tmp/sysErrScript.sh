1924d9ff5dd33c1bdc38a11a66385ff9a79581b7
cdxing <danielxchenx@gmail.com>
2020-09-29 21:14:49 -0400

    dbg

--- tmp/sysErrScript.sh
+++ tmp/sysErrScript.sh
@@ -25,14 +25,14 @@ nNumber=0 # total # of submits
 DEBUG=true
 while [ $nJob1 -le $maxJob1 ]
 do
-    if [$nJob1 == 14] ; then
-      ((nJob1++))
-      continue
-    fi
-    if [$nJob1 == 15] ; then
-      ((nJob1++))
-      continue
-    fi
+    # if [$nJob1 == 14] ; then
+    #   ((nJob1++))
+    #   continue
+    # fi
+    # if [$nJob1 == 15] ; then
+    #   ((nJob1++))
+    #   continue
+    # fi
     echo $nJob1
     nJob2=1
     while [ $nJob2 -le $maxJob2 ]
