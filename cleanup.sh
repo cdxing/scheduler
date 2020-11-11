990d008e74ab5e583e32a4c7de0fb833c627a57f
cdxing <danielxchenx@gmail.com>
2020-11-11 18:08:48 -0500

    move direction

---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+rm -r /star/data01/pwg/dchen/Ana/7p2GeV_FXT_2018_psi2/scheduler/tmp/sched*
+rm /star/data01/pwg/dchen/Ana/7p2GeV_FXT_2018_psi2/scheduler/tmp/*.session.xml
+rm /star/data01/pwg/dchen/Ana/7p2GeV_FXT_2018_psi2/scheduler/out/*
