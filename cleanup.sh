86284edbbd54a28de7e827be4895de3daf114b62
Ding Chen <dchen@rcas6005.rcf.bnl.gov>
2021-07-26 17:11:08 -0400

    cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,19 @@
-rm -r ./tmp/sched*
-rm ./tmp/*.session.xml
-rm ./out/*
-rm ./result/*
+#rm -r ./tmp/sched*
+#rm ./tmp/*.session.xml
+date
+rm -rf  report
+rm -rf  csh
+rm -rf  err
+rm -rf  list
+rm -rf  out
+rm -rf  result
+rm -rf 	sched*
+rm *.dataset
+rm *.session.xml
+mkdir report
+mkdir csh
+mkdir err
+mkdir list
+mkdir out
+mkdir result
+date 
